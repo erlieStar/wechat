c94bf6c0b2394ebfa2d8e0205a90535ad9809a0e
lilimin <290059742@qq.com>
2020-02-26 00:21:31 +0800

    1.修改包名和版本

---
+++ src/main/java/com/javashitang/api/IdAndSecretApi.java
@@ -0,0 +1,6 @@
+package com.javashitang.api;
+
+public class IdAndSecretApi {
+    public static final String appID = "";
+    public static final String appSecret = "";
+}
---
+++ src/main/java/com/javashitang/msg/in/InImageMsg.java
@@ -0,0 +1,10 @@
+package com.javashitang.msg.in;
+
+import java.util.Map;
+
+public class InImageMsg extends InMsg{
+
+    public InImageMsg(Map<String, String> map) {
+        super(map);
+    }
+}
